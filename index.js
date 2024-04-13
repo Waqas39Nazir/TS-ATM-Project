6a150482cb2fdefe1c8373b33d8d3544982e18be
Waqas Nazir <waqasnazir@altaurux.com>
2024-04-13 23:50:19 +0500

    Select Account Type

--- index.js
+++ index.js
@@ -1,2 +1,100 @@
-console.log("CALLED ATM");
-export {};
+#! /usr/bin/node env
+import gradient from "gradient-string";
+import inquirer from "inquirer";
+//PIN Code
+const pinCode = 1122;
+const totalAttempts = 3;
+let pinCodeTried = 1;
+const currentAccBal = 13000;
+const savingAccBal = 19000;
+const incrementPINCodeTriedHandler = () => {
+    pinCodeTried += 1;
+};
+const tryAgainHandler = () => {
+    transactionHandler();
+};
+const balanceInquiryHandler = () => {
+    //show user his selected account balance
+};
+const cashWithdrawHandler = () => {
+    //show user multiples options to withdraw balance...also check current balance and decrement it
+};
+const cashTransferHandler = () => {
+    //transfer cash to another account and decrement account balance
+};
+const selectAccountTypeHandler = () => {
+    inquirer
+        .prompt([
+        {
+            name: "accountType",
+            type: "list",
+            message: gradient.pastel.multiline("Please select an account type:"),
+            choices: [
+                gradient.pastel.multiline("Current Account"),
+                gradient.pastel.multiline("Saving Account"),
+            ],
+        },
+        {
+            name: "action",
+            type: "list",
+            message: gradient.pastel.multiline("Please select an action to perform:"),
+            choices: [
+                gradient.pastel.multiline("Balance Inquiry"),
+                gradient.pastel.multiline("Cash Withdraw"),
+                gradient.pastel.multiline("Cash Transfer"),
+            ],
+        },
+    ])
+        .then((value) => {
+        const selectedAccount = value.accountType.toString();
+        const action = value.action;
+        console.log("Selected Account:", selectedAccount);
+        console.log("Action:", action);
+        if (selectedAccount === gradient.pastel.multiline("Current Account")) {
+            console.log("CALLED 11");
+        }
+        else if (selectedAccount === gradient.pastel.multiline("Saving Account")) {
+            console.log("CALLED 22");
+        }
+    })
+        .catch((error) => {
+        console.log("Error:", error);
+    });
+};
+const transactionHandler = () => {
+    inquirer
+        .prompt([
+        {
+            name: "pinCode",
+            type: "input",
+            message: pinCodeTried === 1
+                ? gradient.pastel.multiline("Enter 4 Digit PIN")
+                : gradient.pastel.multiline("Please enter valid PIN Code"),
+            validate: (value) => {
+                if (isNaN(value)) {
+                    console.log(gradient.pastel.multiline("\nHint:Please enter a 4 digit PIN Code"));
+                    return false;
+                }
+                return true;
+            },
+        },
+    ])
+        .then((value) => {
+        const pin = parseInt(value.pinCode);
+        if (pin === pinCode) {
+            selectAccountTypeHandler();
+        }
+        else if (pin !== pinCode && pinCodeTried !== totalAttempts) {
+            incrementPINCodeTriedHandler();
+            tryAgainHandler();
+        }
+        else if (pinCodeTried === totalAttempts) {
+            console.log(gradient.pastel.multiline("Your ATM Card is blocked, please contact at helpline"));
+        }
+    })
+        .catch((error) => {
+        console.log("Error:", error);
+    });
+};
+// console.log();
+transactionHandler();
